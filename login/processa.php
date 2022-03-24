3b1f4f68cb3799a0090d01cfe6325bc97fbd7c42
Gustavo <gustavo.martins145@etec.sp.gov.br>
2022-03-24 15:25:53 -0300

    Projeto login

---
+++ login/processa.php
@@ -0,0 +1,28 @@
+<?php
+
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
+
+if($usuario == etecmcm && $senha == 141){
+    echo '
+        <script type="text/javascript">
+            alert ("Okay! tudo certo")
+        </script>'    
+    ;
+
+    echo '
+    <script type="text/javascript">
+            window.location = "painel/index.html"
+    </script>'
+    ;
+    }       
+    else {
+        //FALSO
+        //IR PARA A PÁGINA erro.php
+        header('Location: index.html');
+        exit();
+    }
+?>
+
+</body>
+</html>
