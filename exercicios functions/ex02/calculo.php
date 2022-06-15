ccbb4850869f96f7b114e44f623ef6c14aac420d
Gustavo Rodrigues Martins <79873612+projects-gustavo@users.noreply.github.com>
2022-06-15 18:51:02 -0300

    Update calculo.php

--- exercicios functions/ex02/calculo.php
+++ exercicios functions/ex02/calculo.php
@@ -2,7 +2,6 @@
     $num = $_POST['num'];
                 
     if(!empty($num)){
-
         function piramide($num){
             for($i = 0; $i < $num; $i++){
                 for($contador = 1; $contador <= $i+1; $contador++){
