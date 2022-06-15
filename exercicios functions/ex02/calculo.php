f945a14603acfb6021be228fa94a8b143d3f72fb
Gustavo Rodrigues Martins <79873612+projects-gustavo@users.noreply.github.com>
2022-06-15 18:46:11 -0300

    Add files via upload

---
+++ exercicios functions/ex02/calculo.php
@@ -0,0 +1,16 @@
+<?php
+    $num = $_POST['number'];
+                
+    if(!empty($num)){
+
+        function piramide($num){
+            for($i = 0; $i < $num; $i++){
+                for($contador = 1; $contador <= $i+1; $contador++){
+                    echo "$contador ";
+                }
+                echo "<br>";
+            }
+        }
+        piramide($num);                    
+    }
+?>
